fc2e68ae7cb54f9eaa1995f932dc7d59e2010bef
Yanguk <yanguk.heo@knowre.com>
2023-05-15 20:57:15 +0900

    Update: cspell 설정 추가

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,8 +4,9 @@ null_ls.setup({
     sources = {
       null_ls.builtins.formatting.stylua,
   		null_ls.builtins.diagnostics.cspell.with({
-        extra_args = { "--config", vim.fn.expand("%:p:h") .. "/cspell.json" }
+        extra_args = { "--config", vim.fn.expand("%:p:h") .. "/cspell/cspell.json" }
       }),
+      null_ls.builtins.diagnostics.cspell,
   		null_ls.builtins.code_actions.cspell,
     },
 })

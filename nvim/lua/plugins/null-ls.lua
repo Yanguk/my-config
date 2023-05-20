3c5f05372fe6a204cfa58dba044ffedf808a0480
Yanguk <yanguk.heo@knowre.com>
2023-05-20 21:24:37 +0900

    New: lua서버 추가

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,4 +1,5 @@
 local null_ls = require("null-ls")
+local cspell = require("cspell")
 
 local cspell_config = {
 	find_json = function(cwd)
@@ -6,15 +7,9 @@ local cspell_config = {
 	end,
 }
 
-local cspell = require("cspell")
-
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.code_actions.gitsigns,
-
 		cspell.diagnostics.with({ config = cspell_config }),
 		cspell.code_actions.with({ config = cspell_config }),
 	},
 })
-

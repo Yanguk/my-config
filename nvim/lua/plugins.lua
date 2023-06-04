d60a09160f827f34d1441e9884410eb85362edf5
Yanguk <yanguk.heo@knowre.com>
2023-06-05 01:20:10 +0900

    Add formatOn Save

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,217 +5,219 @@ vim.cmd([[
   augroup end
 ]])
 
-return require('packer').startup({function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup({
+	function(use)
+		-- Packer can manage itself
+		use("wbthomason/packer.nvim")
 
-  ------- lsp, cmp
-  use({
-    'neovim/nvim-lspconfig',
-    requires = {
-      {
-        'hrsh7th/cmp-nvim-lsp',
-        config = function ()
-          require('plugins.cmp-nvim')
-        end
-      },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-cmdline' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-vsnip' },
-      { 'hrsh7th/vim-vsnip' },
-    },
-    config = function()
-      require('plugins.lsp')
-    end,
-  })
+		------- lsp, cmp
+		use({
+			"neovim/nvim-lspconfig",
+			requires = {
+				{
+					"hrsh7th/cmp-nvim-lsp",
+					config = function()
+						require("plugins.cmp-nvim")
+					end,
+				},
+				{ "hrsh7th/cmp-buffer" },
+				{ "hrsh7th/cmp-path" },
+				{ "hrsh7th/cmp-cmdline" },
+				{ "hrsh7th/nvim-cmp" },
+				{ "hrsh7th/cmp-vsnip" },
+				{ "hrsh7th/vim-vsnip" },
+			},
+			config = function()
+				require("plugins.lsp")
+			end,
+		})
 
-  use 'editorconfig/editorconfig-vim'
+		use("editorconfig/editorconfig-vim")
 
-  use 'terryma/vim-multiple-cursors'
+		use("terryma/vim-multiple-cursors")
 
-  use ({
-    'simrat39/rust-tools.nvim',
-    config = function()
-      require('plugins.rust-tools')
-    end
-  })
+		use({
+			"simrat39/rust-tools.nvim",
+			config = function()
+				require("plugins.rust-tools")
+			end,
+		})
 
-	use {
-    {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      config = function()
-        require('plugins.treesitter')
-      end
-    },
-    {
-      'windwp/nvim-ts-autotag',
-      after = 'nvim-treesitter',
-    }
-  }
+		use({
+			{
+				"nvim-treesitter/nvim-treesitter",
+				run = ":TSUpdate",
+				config = function()
+					require("plugins.treesitter")
+				end,
+			},
+			{
+				"windwp/nvim-ts-autotag",
+				after = "nvim-treesitter",
+			},
+		})
 
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+		use("JoosepAlviste/nvim-ts-context-commentstring")
 
-  use ({
-    'terrortylor/nvim-comment',
-    config = function()
-      require('plugins.nvim-comment')
-    end
-  })
+		use({
+			"terrortylor/nvim-comment",
+			config = function()
+				require("plugins.nvim-comment")
+			end,
+		})
 
-  -- indent
-  use ({
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require('plugins.indent-blankline')
-    end
-  })
+		-- indent
+		use({
+			"lukas-reineke/indent-blankline.nvim",
+			config = function()
+				require("plugins.indent-blankline")
+			end,
+		})
 
-  use({
-    'gbprod/yanky.nvim',
-    config = function()
-      require('plugins.yanky')
-    end
-  })
+		use({
+			"gbprod/yanky.nvim",
+			config = function()
+				require("plugins.yanky")
+			end,
+		})
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-    after = 'github-nvim-theme',
-    config = function()
-      require('lualine').setup({
-        options = {
-          theme = 'auto'
-        },
-      })
-    end,
-  }
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = { "nvim-tree/nvim-web-devicons" },
+			after = "github-nvim-theme",
+			config = function()
+				require("lualine").setup({
+					options = {
+						theme = "auto",
+					},
+				})
+			end,
+		})
 
-  use { "metalelf0/jellybeans-nvim", requires = "rktjmp/lush.nvim" }
-  use {
-    "ellisonleao/gruvbox.nvim",
-    config = function()
-      vim.o.background = "dark" -- or "light" for light mode
-      vim.cmd([[colorscheme gruvbox]])
-    end
-  }
-  use {
-    'projekt0n/github-nvim-theme',
-    config = function()
-      -- require('github-theme').setup({})
+		use({ "metalelf0/jellybeans-nvim", requires = "rktjmp/lush.nvim" })
+		use({
+			"ellisonleao/gruvbox.nvim",
+			config = function()
+				vim.o.background = "dark" -- or "light" for light mode
+				vim.cmd([[colorscheme gruvbox]])
+			end,
+		})
+		use({
+			"projekt0n/github-nvim-theme",
+			config = function()
+				-- require('github-theme').setup({})
 
-      -- vim.cmd('colorscheme github_dark')
-    end
-  }
-  use {
-    'navarasu/onedark.nvim',
-    config = function()
-      -- require('onedark').load()
-    end
-  }
+				-- vim.cmd('colorscheme github_dark')
+			end,
+		})
+		use({
+			"navarasu/onedark.nvim",
+			config = function()
+				-- require('onedark').load()
+			end,
+		})
 
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require("nvim-tree").setup {}
-    end
-  }
+		use({
+			"nvim-tree/nvim-tree.lua",
+			requires = {
+				"nvim-tree/nvim-web-devicons",
+			},
+			config = function()
+				require("nvim-tree").setup({})
+			end,
+		})
 
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && yarn install",
-    setup = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  })
+		use({
+			"iamcco/markdown-preview.nvim",
+			run = "cd app && yarn install",
+			setup = function()
+				vim.g.mkdp_filetypes = { "markdown" }
+			end,
+			ft = { "markdown" },
+		})
 
-  use {
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('plugins.null-ls')
-		end
-	}
+		use({
+			"jose-elias-alvarez/null-ls.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("plugins.null-ls")
+			end,
+		})
 
-	use {
-		"folke/zen-mode.nvim",
-		config = function()
-			require("zen-mode").setup {}
-		end
-	}
+		use({
+			"folke/zen-mode.nvim",
+			config = function()
+				require("zen-mode").setup({})
+			end,
+		})
 
-	use {
-		"windwp/nvim-autopairs",
-    config = function()
-			require("nvim-autopairs").setup {}
-		end
-  }
+		use({
+			"windwp/nvim-autopairs",
+			config = function()
+				require("nvim-autopairs").setup({})
+			end,
+		})
 
-  use { 'davidmh/cspell.nvim' }
+		use({ "davidmh/cspell.nvim" })
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requires = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('plugins.telescope')
-		end
-	}
+		use({
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.1",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("plugins.telescope")
+			end,
+		})
 
-	use {
-		'lewis6991/gitsigns.nvim',
-		config = function()
-			require('plugins.gitsigns')
-		end
-	}
+		use({
+			"lewis6991/gitsigns.nvim",
+			config = function()
+				require("plugins.gitsigns")
+			end,
+		})
 
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-  use {
-    'kevinhwang91/nvim-ufo',
-    requires = 'kevinhwang91/promise-async',
-    after = 'nvim-treesitter',
-    config = function()
-      require('plugins.nvim-ufo')
-    end
-  }
+		use({
+			"kevinhwang91/nvim-ufo",
+			requires = "kevinhwang91/promise-async",
+			after = "nvim-treesitter",
+			config = function()
+				require("plugins.nvim-ufo")
+			end,
+		})
 
-	use "stevearc/dressing.nvim"
-	use({
-		"ziontee113/icon-picker.nvim",
-		config = function()
-			require("icon-picker").setup({
-				disable_legacy_commands = true
-			})
-		end,
-	})
+		use("stevearc/dressing.nvim")
+		use({
+			"ziontee113/icon-picker.nvim",
+			config = function()
+				require("icon-picker").setup({
+					disable_legacy_commands = true,
+				})
+			end,
+		})
 
-	use {
-		'ojroques/nvim-bufdel',
-		config = function()
-			require('bufdel').setup()
-		end
-	}
+		use({
+			"ojroques/nvim-bufdel",
+			config = function()
+				require("bufdel").setup()
+			end,
+		})
 
-  use {
-		'voldikss/vim-floaterm',
-		config = function()
-			require('plugins.floaterm')
-		end,
-	}
+		use({
+			"voldikss/vim-floaterm",
+			config = function()
+				require("plugins.floaterm")
+			end,
+		})
 
-	use {
-		'sindrets/diffview.nvim',
-		config = function ()
-			require('diffview').setup()
-		end
-	}
+		use({
+			"sindrets/diffview.nvim",
+			config = function()
+				require("diffview").setup()
+			end,
+		})
 
-  -- use 'tpope/vim-fugitive'
-  end
+		-- use 'tpope/vim-fugitive'
+	end,
 })

dea8341c0e5497126e20b6f8b048818e6dbe581c
Yanguk <yanguk.heo@knowre.com>
2023-06-08 21:20:28 +0900

    Update: 신세계 스니펫 추가함
    진작 쓸껄.. 이제 vs코드 보다 좋아졌넹

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -219,5 +219,18 @@ return require("packer").startup({
 		})
 
 		-- use 'tpope/vim-fugitive'
+		use({
+			"L3MON4D3/LuaSnip",
+			-- follow latest release.
+			tag = "v<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			-- install jsregexp (optional!:).
+			run = "make install_jsregexp",
+			config = function ()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+			dependencies = { "rafamadriz/friendly-snippets" }
+		})
+
+		use "rafamadriz/friendly-snippets"
 	end,
 })

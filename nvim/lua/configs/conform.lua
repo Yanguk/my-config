6ddf3c0c93b6b8e2e91203da6273ea4f7338d5a4
Yanguk <yanguk@duck.com>
2024-11-18 21:34:46 +0900

    Remove: slow format

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,6 +1,4 @@
 -- cSpell:disable
-local slow_format_filetypes = {}
-
 -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#format-command
 require("conform").setup({
   formatters_by_ft = {
@@ -41,24 +39,6 @@ require("conform").setup({
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
-
-    if slow_format_filetypes[vim.bo[bufnr].filetype] then
-      return
-    end
-
-    local function on_format(err)
-      if err and err:match("timeout$") then
-        slow_format_filetypes[vim.bo[bufnr].filetype] = true
-      end
-    end
-
-    return { timeout_ms = 300, lsp_format = "fallback" }, on_format
-  end,
-  format_after_save = function(bufnr)
-    if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-      return
-    end
-
-    return { lsp_format = "fallback" }
+    return { timeout_ms = 500, lsp_format = "fallback" }
   end,
 })

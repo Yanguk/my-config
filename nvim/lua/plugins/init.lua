d5f0f9ea904d1c0e6c1cb9a31503e3daac3ef25f
Yanguk <yanguk@duck.com>
2024-11-26 15:30:41 +0900

    Remove: ts-error-translator

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -490,10 +490,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "neovim/nvim-lspconfig",
-      {
-        "dmmulroy/ts-error-translator.nvim",
-        config = true,
-      },
     },
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
     opts = require("configs.typescript-tools"),

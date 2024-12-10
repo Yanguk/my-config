54593a1cd05dbe4a88b03f09584545b9fee61e35
Yanguk <yanguk@duck.com>
2024-12-11 08:32:59 +0900

    Update: lazygit size 조절

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,14 +3,28 @@ require("toggleterm").setup()
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
 
-  vim.keymap.set('t', '<C-x>', [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-x>", [[<C-\><C-n>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, display_name = "lazygit", direction = "float" })
+local Terminal = require("toggleterm.terminal").Terminal
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  display_name = "lazygit",
+  direction = "float",
+  float_opts = {
+    width = function()
+      return vim.o.columns - 10
+    end,
+    height = function()
+      return vim.o.lines - 5
+    end,
+  },
+})
 
 ---@diagnostic disable-next-line: lowercase-global
 function _lazygit_toggle()
@@ -20,11 +34,18 @@ end
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 -- toggleable
-vim.keymap.set({ "n", "t" }, "<A-v>", "<cmd>ToggleTerm direction=vertical size=45<CR>",
-  { desc = "terminal toggleable vertical term" })
+vim.keymap.set(
+  { "n", "t" },
+  "<A-v>",
+  "<cmd>ToggleTerm direction=vertical size=45<CR>",
+  { desc = "terminal toggleable vertical term" }
+)
 
-vim.keymap.set({ "n", "t" }, "<A-h>", "<cmd>ToggleTerm direction=horizontal<CR>",
-  { desc = "terminal toggleable horizontal term" })
+vim.keymap.set(
+  { "n", "t" },
+  "<A-h>",
+  "<cmd>ToggleTerm direction=horizontal<CR>",
+  { desc = "terminal toggleable horizontal term" }
+)
 
-vim.keymap.set({ "n", "t" }, "<A-i>", "<cmd>ToggleTerm direction=float<CR>",
-  { desc = "terminal toggle floating term" })
+vim.keymap.set({ "n", "t" }, "<A-i>", "<cmd>ToggleTerm direction=float<CR>", { desc = "terminal toggle floating term" })

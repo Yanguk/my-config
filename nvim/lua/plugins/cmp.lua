d73daada1eda5699d9276240a03491195337dc19
Yanguk <yanguk@duck.com>
2024-12-13 07:58:15 +0900

    Revert "merge bundle lsp"
    This reverts commit 29f3a4359dd13d4994fb6d7235a4abe0f5ba8c6e.

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,50 @@
+-- blink
+require("blink.cmp").setup({
+  sources = {
+    providers = {
+      copilot = {
+        name = "copilot",
+        module = "blink-cmp-copilot",
+      },
+    },
+    completion = {
+      enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
+    },
+  },
+  completion = {
+    accept = {
+      auto_brackets = {
+        enabled = true,
+      },
+    },
+    list = {
+      max_items = 20,
+    },
+    menu = {
+      border = "rounded",
+      draw = {
+        treesitter = true,
+      },
+    },
+    documentation = {
+      auto_show = true,
+      window = {
+        border = "rounded",
+      },
+    },
+  },
+  snippets = {
+    expand = function(snippet)
+      require("luasnip").lsp_expand(snippet)
+    end,
+    active = function(filter)
+      if filter and filter.direction then
+        return require("luasnip").jumpable(filter.direction)
+      end
+      return require("luasnip").in_snippet()
+    end,
+    jump = function(direction)
+      require("luasnip").jump(direction)
+    end,
+  },
+})

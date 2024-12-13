b89b580121bacc62383204711087498968c38e27
Yanguk <yanguk@duck.com>
2024-12-13 21:13:48 +0900

    remove ghost-text

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -25,13 +25,9 @@ require("blink.cmp").setup({
     },
     documentation = {
       auto_show = true,
-      auto_show_delay_ms = 200,
       window = {
         border = "rounded",
       },
-    },
-    ghost_text = {
-      enabled = true,
     },
   },
   snippets = {

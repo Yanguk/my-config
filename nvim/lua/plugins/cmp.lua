e2c11a9ffd75dfda3b291a947b2ffbcb0e35c0b4
Yanguk <yanguk@duck.com>
2024-12-21 19:45:20 +0900

    feat: update cmp, copilot

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -16,14 +16,66 @@ require("blink.cmp").setup({
     ["<S-Tab>"] = { "snippet_backward", "fallback" },
   },
   sources = {
+    default = { "lsp", "path", "snippets", "buffer", "copilot" },
     providers = {
       copilot = {
         name = "copilot",
         module = "blink-cmp-copilot",
+        score_offset = 100,
+        async = true,
+        transform_items = function(_, items)
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+
+          local last_kind = CompletionItemKind[#CompletionItemKind]
+
+          if last_kind ~= "Copilot" then
+            CompletionItemKind[#CompletionItemKind + 1] = "Copilot"
+          end
+
+          local kind_idx = #CompletionItemKind
+
+          for _, item in ipairs(items) do
+            item.kind = kind_idx
+          end
+
+          return items
+        end,
       },
     },
-    completion = {
-      enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
+  },
+  appearance = {
+    kind_icons = {
+      Copilot = "",
+      Text = "󰉿",
+      Method = "󰊕",
+      Function = "󰊕",
+      Constructor = "󰒓",
+
+      Field = "󰜢",
+      Variable = "󰆦",
+      Property = "󰖷",
+
+      Class = "󱡠",
+      Interface = "󱡠",
+      Struct = "󱡠",
+      Module = "󰅩",
+
+      Unit = "󰪚",
+      Value = "󰦨",
+      Enum = "󰦨",
+      EnumMember = "󰦨",
+
+      Keyword = "󰻾",
+      Constant = "󰏿",
+
+      Snippet = "󱄽",
+      Color = "󰏘",
+      File = "󰈔",
+      Reference = "󰬲",
+      Folder = "󰉋",
+      Event = "󱐋",
+      Operator = "󰪚",
+      TypeParameter = "󰬛",
     },
   },
   completion = {
@@ -34,7 +86,7 @@ require("blink.cmp").setup({
     },
     menu = {
       draw = {
-        treesitter = true,
+        treesitter = { "lsp" },
       },
     },
     documentation = {

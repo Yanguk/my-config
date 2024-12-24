f4e6d4c9f1cc5d051ec0470521377107ccb94a8e
Yanguk <yanguk@duck.com>
2024-12-25 08:33:47 +0900

    update: oil key map 변경

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -25,7 +25,7 @@ require("oil").setup({
   use_default_keymaps = false,
   keymaps = {
     ["g?"] = "actions.show_help",
-    ["<CR>"] = "actions.select",
+    ["<C-y>"] = "actions.select",
     -- ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
     -- ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
     -- ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },

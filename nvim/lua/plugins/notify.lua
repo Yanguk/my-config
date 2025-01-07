20b9e6afffe6bcd77b8e76c25ff83d2e74f42041
Yanguk <yanguk@duck.com>
2025-01-07 14:09:38 +0900

    fix: notify hover issue

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,4 +2,16 @@ require("notify").setup({
   stages = "static",
 })
 
-vim.notify = require("notify")
+local banned_messages = { "No information available" }
+
+-- See https://github.com/neovim/nvim-lspconfig/issues/1931#issuecomment-1297599534
+-- An alternative solution: https://github.com/neovim/neovim/issues/20457#issuecomment-1266782345
+---@diagnostic disable-next-line: duplicate-set-field
+vim.notify = function(msg, ...)
+  for _, banned in ipairs(banned_messages) do
+    if msg == banned then
+      return
+    end
+  end
+  return require("notify")(msg, ...)
+end

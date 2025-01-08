6289e7f95428f2312bbe3503c2d11ded83d1ea22
Yanguk <yanguk@duck.com>
2025-01-08 10:50:48 +0900

    update cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,8 +3,11 @@ require("blink.cmp").setup({
   keymap = {
     preset = "default",
     ["<C-space>"] = {},
-    ["<C-e>"] = { "hide" },
-    ["<C-y>"] = { "select_and_accept" },
+    ["<C-b>"] = {},
+    ["<C-f>"] = {},
+
+    -- ["<C-e>"] = { "hide" },
+    -- ["<C-y>"] = { "select_and_accept" },
 
     ["<C-p>"] = { "select_prev", "fallback" },
     ["<C-n>"] = { "select_next", "fallback" },
@@ -12,9 +15,10 @@ require("blink.cmp").setup({
     ["<C-u>"] = { "scroll_documentation_up", "fallback" },
     ["<C-d>"] = { "scroll_documentation_down", "fallback" },
 
-    ["<Tab>"] = { "snippet_forward", "fallback" },
-    ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    -- ["<Tab>"] = { "snippet_forward", "fallback" },
+    -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
   },
+  snippets = { preset = "luasnip" },
   sources = {
     default = { "lsp", "path", "snippets", "buffer", "copilot" },
     providers = {
@@ -92,19 +96,5 @@ require("blink.cmp").setup({
     documentation = {
       auto_show = true,
     },
-  },
-  snippets = {
-    expand = function(snippet)
-      require("luasnip").lsp_expand(snippet)
-    end,
-    active = function(filter)
-      if filter and filter.direction then
-        return require("luasnip").jumpable(filter.direction)
-      end
-      return require("luasnip").in_snippet()
-    end,
-    jump = function(direction)
-      require("luasnip").jump(direction)
-    end,
   },
 })

17b8b11b9954d5c9f1cc446a75cc4258f19e1f1a
Yanguk <yanguk@duck.com>
2025-01-14 22:32:46 +0900

    add markdown

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -29,21 +29,5 @@ return {
       desc = "Search on current file",
     },
   },
-  build = function()
-    vim.notify("exec build.sh")
-
-    local build_dir = vim.fn.expand("~/.local/share/nvim/lazy/nvim-spectre")
-
-    -- `build_dir`로 이동한 후 `build.sh` 실행
-    local result = vim.fn.system("cd " .. build_dir .. " && bash ./build.sh")
-
-    -- 실행 결과 확인 후 메시지 출력
-    if vim.v.shell_error == 0 then
-      -- 명령이 성공적으로 완료된 경우
-      vim.notify("Build script completed successfully!")
-    else
-      -- 명령이 실패한 경우
-      vim.notify("Error executing build script: " .. result, vim.log.levels.ERROR)
-    end
-  end,
+  build = "bash ./build.sh",
 }

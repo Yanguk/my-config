ef145b33e03021cd4ac8c3908c1811ff020c0a8e
Yanguk <yanguk@duck.com>
2025-01-14 11:22:20 +0900

    add spectre oxi

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -2,6 +2,11 @@ return {
   "nvim-pack/nvim-spectre",
   dependencies = "nvim-lua/plenary.nvim",
   otps = {
+    default = {
+      replace = {
+        cmd = "oxi",
+      },
+    },
     is_block_ui_break = true,
     replace_engine = {
       ["sed"] = {
@@ -24,4 +29,21 @@ return {
       desc = "Search on current file",
     },
   },
+  build = function()
+    vim.notify("exec build.sh")
+
+    local build_dir = vim.fn.expand("~/.local/share/nvim/lazy/nvim-spectre")
+
+    -- `build_dir`로 이동한 후 `build.sh` 실행
+    local result = vim.fn.system("cd " .. build_dir .. " && bash ./build.sh")
+
+    -- 실행 결과 확인 후 메시지 출력
+    if vim.v.shell_error == 0 then
+      -- 명령이 성공적으로 완료된 경우
+      vim.notify("Build script completed successfully!")
+    else
+      -- 명령이 실패한 경우
+      vim.notify("Error executing build script: " .. result, vim.log.levels.ERROR)
+    end
+  end,
 }

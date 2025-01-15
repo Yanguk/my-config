29949d51644bb6b9739363e7599b1dd37ce5968f
Yanguk <yanguk@duck.com>
2025-01-15 12:18:56 +0900

    leap -> flash 대체

--- nvim/lua/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ggandor/leap.nvim",
-  lazy = false,
-  keys = {
-    {"s", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, },
-    {"S", "<Plug>(leap-backward)",mode = { "n", "x", "o" }, },
-    {"gs", "<Plug>(leap-from-window)",mode = { "n", "x", "o" }, }
-  },
-}

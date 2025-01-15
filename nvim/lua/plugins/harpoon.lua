31c465165385bc378036a709cb124ef5574c0a6b
Yanguk <yanguk@duck.com>
2025-01-15 14:04:28 +0900

    fix harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,45 +6,44 @@ return {
     {
       "<leader>ha",
       function()
-        require("harpoon").list():add()
+        require("harpoon"):list():add()
       end,
       desc = "[h]arpoon [a]dd",
     },
     {
       "<C-e>",
       function()
-        require("harpoon.ui").toggle_quick_menu(require("harpoon").list())
+        require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
       end,
       desc = "[h]arpoon m[e]nu",
     },
     {
       "<A-1>",
       function()
-        require("harpoon").list():select(1)
+        require("harpoon"):list():select(1)
       end,
       desc = "harpoon select 1",
     },
     {
       "<A-2>",
       function()
-        require("harpoon").list():select(2)
+        require("harpoon"):list():select(2)
       end,
       desc = "harpoon select 2",
     },
     {
       "<A-3>",
       function()
-        require("harpoon").list():select(3)
+        require("harpoon"):list():select(3)
       end,
       desc = "harpoon select 3",
     },
     {
       "<A-4>",
       function()
-        require("harpoon").list():select(4)
+        require("harpoon"):list():select(4)
       end,
       desc = "harpoon select 4",
     },
   },
 }
-

176d4cb3c48a7bcc0393a40974408ff8e057a8ca
Yanguk <yanguk@duck.com>
2025-01-22 16:23:14 +0900

    feat: add octo

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,13 @@
+return {
+  "pwntester/octo.nvim",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    -- OR 'ibhagwan/fzf-lua',
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = "Octo",
+  opts = {
+    enable_builtin = true,
+  },
+}

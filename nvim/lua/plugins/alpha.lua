724977be98c0bb46308ecd85ea3e4739d1032103
Yanguk <yanguk@duck.com>
2025-01-30 11:00:32 +0900

    alpha title

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,13 +1,26 @@
 return {
   "goolord/alpha-nvim",
-  event = "VimEnter",
-  -- dependencies = { 'echasnovski/mini.icons' },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local startify = require("alpha.themes.startify")
-    -- available: devicons, mini, default is mini
-    -- if provider not loaded and enabled is true, it will try to use another provider
-    startify.file_icons.provider = "devicons"
-    require("alpha").setup(startify.config)
+    local alpha = require("alpha")
+    local theta = require("alpha.themes.theta")
+    local dashboard = require("alpha.themes.dashboard")
+
+    theta.header.val = {
+      [[  ／|_       ]],
+      [[ (o o /      ]],
+      [[  |.   ~.    ]],
+      [[  じしf_,)ノ ]],
+    }
+
+    theta.buttons.val = {
+      { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+      { type = "padding", val = 1 },
+      dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+      dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+      dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+    }
+
+    alpha.setup(theta.config)
   end,
 }

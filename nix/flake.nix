f8022fd8d422592e9506338216162d233979aab8
Yanguk <yanguk@duck.com>
2025-02-09 21:44:05 +0900

    wip: nix로 변경...

---
+++ nix/flake.nix
@@ -0,0 +1,136 @@
+{
+  description = "Yanguk nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+    # home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config = {
+            allowUnfree = true;
+          };
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.alacritty
+            pkgs.neovim
+            pkgs.tmux
+            pkgs.obsidian
+            pkgs.nixfmt-rfc-style
+            pkgs.lazygit
+          ];
+
+          fonts.packages = with pkgs; [
+            nerd-fonts.hack
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+            ];
+            casks = [
+              "duckduckgo"
+            ];
+            masApps = {
+              "kakaotalk" = 869223134;
+            };
+            onActivation.cleanup = "zap";
+            onActivation.autoUpdate = true;
+            onActivation.upgrade = true;
+          };
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          system.defaults = {
+          };
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Yanguk
+      darwinConfigurations."Yanguk" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "yanguk";
+
+              # autoMigrate = true;
+            };
+          }
+          # home-manager.darwinModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #
+          #   users.users.yanguk = {
+          #     name = "yanguk";
+          #     home = "/Users/yanguk";
+          #   };
+          #
+          #   home-manager.users.yanguk = {
+          #     home.stateVersion = "24.11";
+          #     programs = {
+          #       zsh = {
+          #         enable = true;
+          #         # custom = "$HOME/.config/zsh/omz-custom";
+          #         oh-my-zsh = {
+          #           enable = true;
+          #         };
+          #         # dotDir = ".config/zsh";
+          #       };
+          #       autojump = {
+          #         enable = true;
+          #       };
+          #
+          #       direnv = {
+          #         enable = true;
+          #         nix-direnv.enable = true;
+          #       };
+          #     };
+          #   };
+          # }
+        ];
+      };
+    };
+}
